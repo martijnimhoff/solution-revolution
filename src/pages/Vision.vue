adff3638ed4a7f37706800d626163f8c0d84b1d9
Gilian Baracs <gbaracs@ebuddy.com>
2020-03-21 20:46:54 +0100

    added more text to pages

--- src/pages/Vision.vue
+++ src/pages/Vision.vue
@@ -10,8 +10,20 @@
             <!-- Content -->
             <article id="content">
               <header>
-                <h2>What if Covid-19 is an open call for a better world?</h2>
-                <p>Coming soon ...</p>
+                <h2>The Vision</h2>
+                <p><br>The COVID-19 crisis provides an unprecedented window of opportunity to present alternative models of economic and social governance to our society. <br><br>
+
+A global pause button has been pushed. Driven out of their comfort zone, people are forced to act in unhabitual ways, exploring new angles of perspective. <br><br>
+
+It is up to all of us to determine what happens next - in six months from now, do we want the current economic system to resume its course? Or  can we introduce a smarter, more sustainable and fairer alternative? A more logical way?<br><br>
+
+One of the things that makes global change difficult for us humans is the daily reality of our economy - in our personal lives and a global level. But guess what, that has now temporarily been put out of order! <br><br>
+
+
+By using the power of the internet, we bring together creative and logical thinkers, young and old, that form the jigsaw pieces of this puzzle. If inspired people exchange their ideas, logical solutions will appear.  <br><br>
+
+
+</p>
               </header>
             </article>
           </div>
