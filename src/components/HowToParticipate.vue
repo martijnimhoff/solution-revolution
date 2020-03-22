66139a15242d69e2a21dabf4ce4856956a88d118
Gilian Baracs <gbaracs@ebuddy.com>
2020-03-22 21:35:25 +0100

    updated Participation page

--- src/components/HowToParticipate.vue
+++ src/components/HowToParticipate.vue
@@ -4,27 +4,40 @@
       <h2>How to participate</h2>
     </header>
     <div class="row">
+    
       <section class="col-4 col-12-narrower feature">
         <font-awesome-icon size="2x" icon="video"/>
         <header>
           <h3>Record your video</h3>
         </header>
-        <p>Mention who nominated you. State your problem, and that that is bad news! Then state your solution, and that
-          that is good news!</p>
+      
+                    <li align="left">Introduce yourself!</li>
+      
+                    <li align="left">Mention who nominated you and that this is the #solutionrevolution!</li>
+                 
+
+                    <li align="left">State your challenge due to the current situation</li>
+            
+                    <li align="left">State your solution to this problem!</li>
+
+
       </section>
+     
+     
       <section class="col-4 col-12-narrower feature">
         <font-awesome-icon size="2x" icon="user-friends"/>
         <header>
           <h3>Nominate others</h3>
         </header>
-        <p>Nominate one or more other persons to share their good news solution!</p>
+        <p>Nominate one or more other persons to share their solution!</p>
       </section>
+  
       <section class="col-4 col-12-narrower feature">
         <font-awesome-icon size="2x" icon="share-alt"/>
         <header>
           <h3>Share</h3>
         </header>
-        <p>Post your video to your favorite social media channel. Be sure to use the hashtags #solutionrevolution and #goodnews.</p>
+        <p>Post your video to your favorite social media channel. Be sure to use the hashtag #solutionrevolution!</p>
       </section>
     </div>
   </div>
