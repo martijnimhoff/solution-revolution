1b6d6c26b927412ae27c07335ba1b6dafb1cfdb0
Martijn Imhoff <info@martijnimhoff.nl>
2020-03-24 17:02:30 +0100

    cant test it locally

--- pages/404.vue
+++ pages/404.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
-    <!-- Stub for github pages. 404 is actually loaded via the error.vue -->
+    <!-- Main -->
+    <div class="wrapper">
+      <div id="main" class="container">
+        <div class="row">
+          <div class="col-10 off-1">
+            <!-- Content -->
+            <article>
+              <header>
+                <h2>This page could not be found</h2>
+              </header>
+              <p>
+                <nuxt-link to="/">
+                  Return to homepage
+                </nuxt-link>
+              </p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Vision',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Read more about the vision behind the #SolutionRevolution.'
+        }
+      ]
+    }
+  }
+}
+</script>
